51c7eb51991d22b573e186cf9e746ba5574281dd
Pavel Strelchenko <pstrelchenkoHH@gmail.com>
2018-12-13 13:10:36 +0300

    Каркас для странички выбора модулей-библиотек

---
+++ src/main/java/ru/hh/android/plugin/feature_module/component/module/ModuleInteractor.kt
@@ -0,0 +1,18 @@
+package ru.hh.android.plugin.feature_module.component.module
+
+import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.project.Project
+
+class ModuleInteractor(
+        private val project: Project
+) : ProjectComponent {
+
+    fun getLibrariesModules() {
+
+    }
+
+    fun getApplicationModules() {
+
+    }
+
+}
---
+++ src/main/java/ru/hh/android/plugin/feature_module/wizard/step/choose_modules/ChooseModulesView.kt
@@ -0,0 +1,5 @@
+package ru.hh.android.plugin.feature_module.wizard.step.choose_modules
+
+import ru.hh.android.core.BaseView
+
+interface ChooseModulesView : BaseView
