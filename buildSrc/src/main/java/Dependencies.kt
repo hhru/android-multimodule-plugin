93c781e7728cabf4b050a6d2844a325e1da24cde
Pavel Strelchenko <p.strelchenko@hh.ru>
2019-10-16 15:55:37 +0300

    [MOB-11094] -- Добавил в проект buildSrc модуль с версиями библиотек

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,21 @@
+
+object Versions {
+
+    // TODO - Value is duplicated in buildSrc/gradle.properties
+    const val kotlinVersion = "1.3.50"
+
+    const val intellijPluginVersion = "0.4.10"
+    const val freeMarkerVersion = "2.3.29"
+    const val commonsIOVersion = "2.4"
+    const val moxyVersion = "1.5.5"
+
+}
+
+object Libs {
+
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}"
+    const val freeMarker = "org.freemarker:freemarker:${Versions.freeMarkerVersion}"
+    const val commonsIO = "commons-io:commons-io:${Versions.commonsIOVersion}"
+    const val moxy = "com.arello-mobile:moxy:${Versions.moxyVersion}"
+
+}
