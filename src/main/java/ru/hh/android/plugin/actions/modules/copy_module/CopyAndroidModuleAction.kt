b92e4c5ac6a5174f021b5fc63cf3b0475af336ba
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-04-29 14:47:16 +0300

    [MOB-13356__action_for_module_copy] -- Заготовка action-а для копирования модуля

---
+++ src/main/java/ru/hh/android/plugin/actions/modules/copy_module/CopyAndroidModuleAction.kt
@@ -0,0 +1,16 @@
+package ru.hh.android.plugin.actions.modules.copy_module
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import ru.hh.android.plugin.actions.AndroidModuleAction
+
+
+/**
+ * Action for copy module.
+ */
+class CopyAndroidModuleAction : AndroidModuleAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        // TODO
+    }
+
+}
