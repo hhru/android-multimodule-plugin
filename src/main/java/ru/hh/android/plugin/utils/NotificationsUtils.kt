df88a2ad9cd99529210f1f6e7852d3f312fdd1cf
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-05-12 12:52:39 +0300

    [master] -- Убрал лишний balloon в нотификации

--- src/main/java/ru/hh/android/plugin/utils/NotificationsUtils.kt
+++ src/main/java/ru/hh/android/plugin/utils/NotificationsUtils.kt
@@ -5,11 +5,9 @@ import ru.hh.android.plugin.services.NotificationsFactory
 
 
 fun Project.notifyInfo(message: String) {
-    logInfo(message)
     NotificationsFactory.getInstance(this).info(message)
 }
 
 fun Project.notifyError(message: String) {
-    logError(message)
     NotificationsFactory.getInstance(this).error(message)
 }
