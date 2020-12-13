17be6bca1588a2a232c34da28bcf50e009f9a13e
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-12-13 12:45:23 +0400

    [MOB-15749] -- Fixes for 'mkDirs' command

--- shared/features/geminio-sdk/src/main/kotlin/ru/hh/plugins/geminio/sdk/template/executors/MkDirsCommandExecutor.kt
+++ shared/features/geminio-sdk/src/main/kotlin/ru/hh/plugins/geminio/sdk/template/executors/MkDirsCommandExecutor.kt
@@ -1,6 +1,7 @@
 package ru.hh.plugins.geminio.sdk.template.executors
 
 import com.android.tools.idea.wizard.template.RecipeExecutor
+import ru.hh.plugins.extensions.EMPTY
 import ru.hh.plugins.geminio.sdk.recipe.models.commands.MkDirItem
 import ru.hh.plugins.geminio.sdk.recipe.models.commands.RecipeCommand
 import ru.hh.plugins.geminio.sdk.template.mapping.expressions.evaluateString
@@ -13,22 +14,26 @@ internal fun RecipeExecutor.execute(
     executorData: GeminioRecipeExecutorData
 ) {
     for (rootItem in command.dirs) {
-        makeDirectories(rootItem, executorData)
+        makeDirectories(rootItem, String.EMPTY, executorData)
     }
 }
 
-private fun RecipeExecutor.makeDirectories(mkDirItem: MkDirItem, executorData: GeminioRecipeExecutorData) {
-    val filePath = requireNotNull(
+private fun RecipeExecutor.makeDirectories(
+    mkDirItem: MkDirItem,
+    combinedPath: String,
+    executorData: GeminioRecipeExecutorData
+) {
+    val filePath = "$combinedPath/".takeIf { combinedPath.isNotEmpty() }.orEmpty() + requireNotNull(
         mkDirItem.name.evaluateString(
             executorData.moduleTemplateData,
             executorData.existingParametersMap
         )
     ) {
-        "Recipe execution, 'mkDirs' command: Error with directory name evaluation [mkDirItem: $mkDirItem, executorData: $executorData]"
+        "Recipe execution, 'mkDirs' command: Error with directory name evaluation [mkDirItem: $mkDirItem, combinedPath: $combinedPath, executorData: $executorData]"
     }
     createDirectory(File(filePath))
 
     for (subdirectoryItem in mkDirItem.subDirs) {
-        makeDirectories(subdirectoryItem, executorData)
+        makeDirectories(subdirectoryItem, filePath, executorData)
     }
 }
