ea86fd559861f6dd4b6647365031709fb5c56e8e
Nikita Kulikov <nikita@kulikof.ru>
2023-07-27 15:59:16 +0400

    Bump java version

--- libraries/src/main/kotlin/ru/hh/plugins/ExternalLibrariesExtension.kt
+++ libraries/src/main/kotlin/ru/hh/plugins/ExternalLibrariesExtension.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 
 abstract class ExternalLibrariesExtension @Inject constructor(private val providers: ProviderFactory) {
 
-    val javaVersion = JavaVersion.VERSION_11
+    val javaVersion = JavaVersion.VERSION_17
     val chosenIdeaVersion: Product = Product.LocalIde(
         pathToIde = systemProperty("androidStudioPath").get(),
         compilerVersion = systemProperty("androidStudioCompilerVersion").get(),
