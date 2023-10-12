bbaed2ccd20dd1393b5309d7204512d6d2406484
daniiltimonin <rand1kch@gmail.com>
2023-10-12 22:00:22 +0200

    fix add module via psi

--- shared/feature/geminio-sdk/src/main/kotlin/ru/hh/plugins/geminio/sdk/template/executors/AddDaggerModuleCommandExecutor.kt
+++ shared/feature/geminio-sdk/src/main/kotlin/ru/hh/plugins/geminio/sdk/template/executors/AddDaggerModuleCommandExecutor.kt
@@ -1,8 +1,20 @@
 package ru.hh.plugins.geminio.sdk.template.executors
 
+import com.android.tools.idea.kotlin.findValueArgument
+import com.android.tools.idea.lint.common.findAnnotation
+import com.android.tools.idea.lint.common.isAnnotationTarget
 import com.android.tools.idea.wizard.template.RecipeExecutor
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.codeStyle.CodeStyleManager
+import org.jetbrains.kotlin.idea.completion.argList
 import org.jetbrains.kotlin.idea.core.util.toPsiDirectory
+import org.jetbrains.kotlin.idea.gradleTooling.get
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtPsiFactory
 import ru.hh.plugins.geminio.sdk.recipe.models.commands.RecipeCommand
 import ru.hh.plugins.geminio.sdk.template.mapping.expressions.evaluateString
 import ru.hh.plugins.geminio.sdk.template.models.GeminioRecipeExecutorData
@@ -14,35 +26,49 @@ internal fun RecipeExecutor.execute(
     command: RecipeCommand.AddDaggerModule,
     executorData: GeminioRecipeExecutorData
 ) = with(executorData) {
-    HHLogger.d("AddDaggerModule command [$command], isDryRun: ${executorData.isDryRun}")
-    if (executorData.isDryRun) {
-        HHLogger.d("\tExecute only when isDryRun == true")
+    HHLogger.d("AddDaggerModule command [$command]")
 
-        val daggerAppModulePath = command.daggerAppModulePath.evaluateString(
-            targetDirectory,
-            moduleTemplateData,
-            existingParametersMap
-        ) ?: return@with
+    val daggerAppModulePath = command.daggerAppModulePath.evaluateString(
+        targetDirectory,
+        moduleTemplateData,
+        existingParametersMap
+    ) ?: return@with
 
-        val appComponentName = command.appComponentName.evaluateString(
-            targetDirectory,
-            moduleTemplateData,
-            existingParametersMap
-        ) ?: return@with
+    val appComponentName = command.appComponentName.evaluateString(
+        targetDirectory,
+        moduleTemplateData,
+        existingParametersMap
+    ) ?: return@with
 
-        val featureModuleName = command.featureModuleName.evaluateString(
-            targetDirectory,
-            moduleTemplateData,
-            existingParametersMap
-        ) ?: return@with
+    val featureModuleName = command.featureModuleName.evaluateString(
+        targetDirectory,
+        moduleTemplateData,
+        existingParametersMap
+    ) ?: return@with
 
-        featureModuleName.let {
-            val appComponentPath =
-                File(daggerAppModulePath).toPsiDirectory(executorData.project) ?: return@let
-            val appComponentPsi = appComponentPath.findFile(appComponentName)
-            HHLogger.e(appComponentPsi?.children.toString())
-            HHLogger.e(appComponentPsi?.children?.map { it.text }.toString())
-            appComponentPsi?.children?.find { it.text == "CLASS" }
-        }
+    val path = "${executorData.project.basePath}/${daggerAppModulePath}"
+
+    val appComponentPath = File(path).toPsiDirectory(executorData.project) ?: return
+
+    val appComponentPsi = appComponentPath.findFile(appComponentName) as KtFile
+
+    val annotationTargetPsi = appComponentPsi.children.find { it.isAnnotationTarget() } as KtClass
+    val componentAnnotation =
+        annotationTargetPsi.annotationEntries.find { it.text.contains("@Component") } ?: return@with
+
+    val argument =
+        componentAnnotation.findValueArgument("modules")?.getArgumentExpression() ?: return@with
+    val modulePsiFactory = KtPsiFactory(argument.project)
+
+    argument.addBefore(modulePsiFactory.createComma(), argument.lastChild)
+    argument.addBefore(modulePsiFactory.createNewLine(), argument.lastChild)
+    argument.children.forEach {
+        println("SOSI ${it.text}")
     }
+    argument.addBefore(
+        modulePsiFactory.createArgument("${featureModuleName}::class"),
+        argument.lastChild
+    )
+
+    CodeStyleManager.getInstance(project).reformat(appComponentPsi)
 }
